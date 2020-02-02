db68688ea3c5406b7cd484853ffceb9dde0df5f9
Szymon Hajnos <rodetroner@gmail.com>
2020-02-02 13:34:46 +0100

    Initial commit

---
+++ jumper.py
@@ -0,0 +1,11 @@
+import sys, pygame
+pygame.init()
+
+size = width, height = 320, 240
+
+screen = pygame.display.set_mode(size)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
