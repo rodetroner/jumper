e472c09c005580c328914f798b38fbb0984ceec8
Szymon Hajnos <rodetroner@gmail.com>
2021-08-01 17:29:32 +0200

    Update a file's path

--- jumper.py
+++ jumper.py
@@ -5,7 +5,7 @@ size = width, height = 320, 240
 class Jumper(pygame.sprite.Sprite):
     def __init__(self, screen):
         self.screen = screen
-        self.image = pygame.image.load('jumper.png')
+        self.image = pygame.image.load('images/jumper.png')
         self.rect = self.image.get_rect()
 
     def blit(self):
